7b9968976da589a0985663e933f6b9c10e0bfe89
Teodoros2 <186217124+Teodoros2@users.noreply.github.com>
2024-11-13 00:00:00 +0500

    'add file evaluator.c'

---
+++ evaluator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "evaluator.h"
+
+int evaluate(Node *ast) {
+    // Simple evaluation logic (not fully implemented)
+    // Return the result of the expression
+}
