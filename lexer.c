73a33bbe18165a1904e3397700996ded77af4112
Teodoros2 <186217124+Teodoros2@users.noreply.github.com>
2024-11-13 00:00:00 +0500

    'add file lexer.c'

---
+++ lexer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lexer.h"
+
+Token *tokenize(const char *input) {
+    // Simple tokenization logic (not fully implemented)
+    // Return an array of tokens
+}
