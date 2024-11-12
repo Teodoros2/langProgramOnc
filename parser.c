90c7d74f15f2fc0508437725df24de50207420d1
Teodoros2 <186217124+Teodoros2@users.noreply.github.com>
2024-11-13 00:00:00 +0500

    'add file parser.c'

---
+++ parser.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
+
+Node *parse(Token *tokens) {
+    // Simple parsing logic (not fully implemented)
+    // Return an AST or similar structure
+}
